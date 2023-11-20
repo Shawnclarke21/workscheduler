67355372af0a29915e33377a33aff0b4ce2a5256
shawn <srclarke777@gmail.com>
2023-11-19 22:26:11 -0700

    added in js code to make it so that my code stays in the local storage

--- script.js
+++ script.js
@@ -2,18 +2,24 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-const localStorage = {};
-dayjs.locale(localStorage);
+const local = {};
+dayjs.locale(local);
 
 $(function () {
-  
+  const hour = dayjs().format('H');
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-
+function saveText(){
+  $('.saveBtn').on('click', function(){
+    const key = $(this).parent().attr('id');
+    const value = $(this).siblings('.description').val();
+    localStorage.setItem(key,value);
+  });
+}
 
     //
     // TODO: Add code to apply the past, present, or future class to each time
@@ -27,6 +33,8 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+
+    saveText();
   });
   
   
